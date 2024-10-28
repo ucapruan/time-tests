dca85b6e7e275f22eea3179cbd10f2fa209b0cb6
Shiyi <sgsruan616@gmail.com>
2024-10-28 11:16:38 +0000

    add negative test

--- test_times.py
+++ test_times.py
@@ -1,4 +1,5 @@
 from times import compute_overlap_time, time_range
+from pytest import raises
 
 def test_generic_case():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
@@ -24,3 +25,7 @@ def test_third_case():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
+
+def test_negative_case():
+    with raises(ValueError):
+        time_range("2010-01-12 13:00:00", "2010-01-12 12:00:00")
