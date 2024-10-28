b3abe9ada534c7f983d94251f720cd9427ef8a0b
Shiyi <sgsruan616@gmail.com>
2024-10-28 10:28:32 +0000

    Correct times and add tests

--- test_times.py
+++ test_times.py
@@ -5,3 +5,22 @@ def test_generic_case():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert compute_overlap_time(large, short) == expected
+
+def test_first_case():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00", 2, 60)
+    expected = []
+    assert compute_overlap_time(large, short) == expected
+
+def test_second_case():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00", 2, 60)
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected
+
+def test_third_case():
+    # tbc
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
+    expected = [("2010-01-12 10:30:00","2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    assert compute_overlap_time(large, short) == expected
